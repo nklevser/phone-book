c08a58e2635f013fd7ea9fc418a93507e4148735
nklevser <klevnikita6@gmail.com>
2024-05-25 16:50:07 +0300

    Add files via upload

---
+++ phone book.py
@@ -0,0 +1,23 @@
+book = {}
+while True:
+ user=input()
+ if user=='add':
+  name=input('name')
+  tele=input('tele')
+  book[name]=tele
+ if user=='del':
+  name=input('имя')
+  if name in book:
+   del book[name]
+   print('спешно удаленно')
+  else:
+   print('ошибка')
+
+ if user=='print':
+  print(book)
+ if user=='poisk':
+  name=input('name')
+  if  name in book:
+   print(book[name])
+  else:
+   print('ошибка')
